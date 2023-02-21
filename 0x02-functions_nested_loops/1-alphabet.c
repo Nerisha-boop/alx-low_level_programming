083803fb8331063412d6b0241808d79c16970b68
Nerisha-boop <nerisha66@gmail.com>
2023-02-21 09:44:16 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char C = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
