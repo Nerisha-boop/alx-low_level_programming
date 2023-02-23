ebaa81cfc4a8597cb0486a4e9b8419820bca4038
Nerisha-boop <nerisha66@gmail.com>
2023-02-23 05:37:48 -0800

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,12 +6,12 @@
  * Return: 1 for upper letter or 0 for any else
  */
 
-int _isupper(int x)
+int _isupport(int x)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+
+	if (x >= 65 && x <= 90)
 	{
 		return (1);
 	}
-	else
 	return (0);
 }
