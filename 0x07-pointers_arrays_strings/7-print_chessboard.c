dbf44d5c753e5511fc86d3474baa8c6417d20f4a
Nerisha-boop <nerisha66@gmail.com>
2023-03-06 11:37:36 -0800

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -3,18 +3,19 @@
 
 /**
  * print_chessboard - Entry point
- * @: array
+ * @a: array
  * Return: Always 0 (Success)
  */
 
 void print_chessboard(char (*a)[8])
 {
-	int i, n;
+	int i;
+	int j;
 
 	for (i = 0; i < 8; i++)
 	{
-		for (n = 0; n < 8; n++)
-			_putchar(a[i][n]);
-		_putchar('\n');
+		for (j = 0; j < 8; j++)
+			putchar(a[i][j]);
+		putchar('\n');
 	}
 }
